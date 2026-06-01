c6808111815b0d8b816254e0f0694f7511e97584
NamPham14 <nampham2662@gmail.com>
2026-06-02 02:15:43 +0700

    ok

--- src/main/java/com/trip4hanoi/app/config/SecurityConfig.java
+++ src/main/java/com/trip4hanoi/app/config/SecurityConfig.java
@@ -63,7 +63,9 @@ public class SecurityConfig {
             "/api/posts/**",
             "/api/categories/**",
             "/api/comments/post/**",
-            "/api/weather"
+            "/api/weather",
+            "/api/itineraries/featured",
+            "/api/itineraries/samples"
     };
 
 
